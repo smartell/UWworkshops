2eb03f7dc5fa15f2c0a7a505844ee5b35deebdc1
Steve Martell <martell.steve@gmail.com>
2014-02-07 14:09:57 -0800

    Added selex.hpp

---
+++ src/catage/selex.hpp
@@ -0,0 +1,19 @@
+//selex.hpp
+
+#ifndef SELEX_HPP
+#define SELEX_HPP
+
+#include <admodel.h>
+
+namespace slx {
+	template<class T>
+	class Selex
+	{
+	public:
+		
+	}
+
+}//slx
+
+
+#endif /* SELEX_HPP */
